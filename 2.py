81d27cf0008ec278eb59848bb51f847943f02cbc
HuseyinYigitUSTA <147235923+HuseyinYigitUSTA@users.noreply.github.com>
2023-10-07 11:02:46 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+n = input("Enter a integeer : ")
+
+n = int(n)
+
+if n%2 == 1 :
+    print("Weird")
+
+if n%2==0 and 2<=n and n<=5 :
+    print("Not Weird")
+
+if n%2==0 and 6<=n and n<=20 :
+    print("Weird")
+
+if n%2==0 and 20<n :
+    print("Not Weird")
